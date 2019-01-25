1e7efe11734c9aea82406a7bdfabe59775f729e4
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-25 21:43:53 +0530

    code structure changes

--- Contents.swift
+++ Contents.swift
@@ -7,16 +7,22 @@ extension Date {
     func displayTimeInSocialMediaStyle() ->  String {
         let secondsAgo = Int(Date().timeIntervalSince(self))
         
-        if secondsAgo < 60 {
+        let minute = 60
+        let hour = 60 * minute
+        let day = 24 * hour
+        let week = 7 * day
+        
+        
+        if secondsAgo < minute {
             return "\(secondsAgo) second ago"
-        } else if secondsAgo < 60 * 60 {
-            return "\(secondsAgo / 60) minutes ago"
-        } else if secondsAgo < 60 * 60 * 24 {
-            return "\(secondsAgo / 60 / 60) hours ago"
-        } else if secondsAgo < 60 * 60 * 24 * 7 {
-            return "\(secondsAgo / 60 / 60 / 24) days ago"
+        } else if secondsAgo < hour {
+            return "\(secondsAgo / minute) minutes ago"
+        } else if secondsAgo < day {
+            return "\(secondsAgo / hour) hours ago"
+        } else if secondsAgo < week {
+            return "\(secondsAgo / day) days ago"
         }
-        return "\(secondsAgo / 60 / 60 / 24 / 7) weeks ago"
+        return "\(secondsAgo / week) weeks ago"
     }
 }
 
